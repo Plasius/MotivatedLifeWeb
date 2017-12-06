68266d9c37e2d47d916b8b0a83e62d2b70a3956e
Plasius <Plasius@users.noreply.github.com>
2017-12-06 15:59:34 +0200

    We accept usernames!
    Quick fix so the user has to provide a username, an email and a password.

--- accounts/views.py
+++ accounts/views.py
@@ -7,49 +7,57 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
+# /register
 def register_view(request):
 	c= {}
 	c.update(csrf(request))
 	c['title']= 'Register'
 	return render(request, 'accounts/register.html', c)
 
+# /login
 def login_view(request):
 	c= {}
 	c.update(csrf(request))
 	c['title']= 'Login'
 	return render(request, 'accounts/login.html', c)
-	
+
+# /auth -form
 def auth_view(request, onsuccess='/dashboard/', onfail='/fail'):
-    user = authenticate(username=request.POST.get('email'), password=request.POST.get('password'))
+    user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
     if user is not None:
         login(request, user)
         return redirect(onsuccess)
     else:
-        return redirect(onfail)  
-	
-		
+        return redirect(onfail)
 
 def create_user(username, email, password):
     user = User(username=username, email=email)
     user.set_password(password)
     user.save()
     return user
-	
-def user_exists(username):
-    user_count = User.objects.filter(username=username).count()
+
+def user_email_exists(email):
+    user_count = User.objects.filter(username=email).count()
     if user_count == 0:
         return False
     return True
-	
+
+def user_name_exists(name):
+    user_count = User.objects.filter(username=name).count()
+    if user_count == 0:
+        return False
+    return True
+
+# /signup -form
 def signup_view(request):
 	post = request.POST
-	if not user_exists(post['email']):
-		user = create_user(username=post['email'], email=post['email'], password=post['password'])
+	if not user_email_exists(post['email']) and not user_name_exists(post['username']):
+		user = create_user(username=post['username'], email=post['email'], password=post['password'])
 		return auth_view(request)
 	else:
 		return redirect("/account/login/")
-		
+
+# /logout
 def logout_view(request):
 	logout(request)
 	return redirect('/')
-		
